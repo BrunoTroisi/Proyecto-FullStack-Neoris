df7c44c8a9efea0a646338bdaf3fdbd5ace358d1
LanzDaniel <lanzdaniel8@gmail.com>
2021-11-20 09:14:19 -0300

    Update bd-juegos.sql
    Base de datos:
    modifique:
    Tabla:
    -juego: agregue el genero
    -venta: agregue su respectivas relaciones con ambas tablas y su contador.

--- bd-juegos.sql
+++ bd-juegos.sql
@@ -1,125 +1,138 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 16-11-2021 a las 16:10:13
--- Versión del servidor: 10.4.21-MariaDB
--- Versión de PHP: 7.3.31
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: tienda
+-- ------------------------------------------------------
+-- Server version	8.0.27
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de datos: `bd-juegos`
+-- Table structure for table `biblioteca`
 --
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `biblioteca`
---
-
+DROP TABLE IF EXISTS `biblioteca`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `biblioteca` (
-  `id_biblioteca` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `juegos`
---
-
-CREATE TABLE `juegos` (
-  `idjuego` int(11) NOT NULL,
-  `titulo` text COLLATE utf8_bin NOT NULL,
-  `descripcion` text COLLATE utf8_bin NOT NULL,
-  `edad_sugerida` date NOT NULL,
-  `nombre_desarrollador` text COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- --------------------------------------------------------
+  `idBiblioteca` int NOT NULL AUTO_INCREMENT,
+  `nombreJuegos` tinytext,
+  PRIMARY KEY (`idBiblioteca`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `usuario`
+-- Dumping data for table `biblioteca`
 --
 
+LOCK TABLES `biblioteca` WRITE;
+/*!40000 ALTER TABLE `biblioteca` DISABLE KEYS */;
+/*!40000 ALTER TABLE `biblioteca` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `juego`
+--
+
+DROP TABLE IF EXISTS `juego`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `juego` (
+  `idJuego` int NOT NULL AUTO_INCREMENT,
+  `titulo` tinytext,
+  `genero` tinytext,
+  `precio_base` decimal(4,0) DEFAULT NULL,
+  `descripcion` text,
+  `nombre_desarrollador` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idJuego`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `juego`
+--
+
+LOCK TABLES `juego` WRITE;
+/*!40000 ALTER TABLE `juego` DISABLE KEYS */;
+/*!40000 ALTER TABLE `juego` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuario` (
-  `id_usuario` int(11) NOT NULL,
-  `nombre` text COLLATE utf8_bin NOT NULL,
-  `apellido` text COLLATE utf8_bin NOT NULL,
-  `id_publica` int(11) NOT NULL,
-  `email` varchar(60) COLLATE utf8_bin NOT NULL,
-  `fecha_nacimiento` date NOT NULL,
-  `contraseña` varchar(60) COLLATE utf8_bin NOT NULL,
-  `pais_region` text COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- --------------------------------------------------------
+  `idUsuario` int NOT NULL AUTO_INCREMENT,
+  `nombre` tinytext,
+  `apellido` tinytext,
+  `idPublica` varchar(10) DEFAULT NULL,
+  `email` varchar(15) DEFAULT NULL,
+  `fechaNacimiento` date DEFAULT NULL,
+  `pais` tinytext,
+  `region` tinytext,
+  PRIMARY KEY (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `venta`
+-- Dumping data for table `usuario`
 --
 
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `venta`
+--
+
+DROP TABLE IF EXISTS `venta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `venta` (
-  `id_venta` int(11) NOT NULL,
-  `id_publica` int(11) NOT NULL,
-  `fecha` datetime NOT NULL,
-  `total` decimal(10,0) NOT NULL,
-  `cantidad_juegos` int(11) NOT NULL,
-  `id_juegos` int(11) NOT NULL,
-  `medio_pago` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`medio_pago`))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `idVenta` int NOT NULL AUTO_INCREMENT,
+  `fecha` datetime(6) DEFAULT NULL,
+  `cantidad` int DEFAULT NULL,
+  `total` decimal(6,0) DEFAULT NULL,
+  `idPublica` int NOT NULL,
+  `idJuego` int NOT NULL,
+  `idBiblioteca` int DEFAULT NULL,
+  PRIMARY KEY (`idVenta`),
+  KEY `id_venta_publica_idx` (`idPublica`),
+  KEY `id_venta_juego_idx` (`idJuego`),
+  CONSTRAINT `id_venta_juego` FOREIGN KEY (`idJuego`) REFERENCES `juego` (`idJuego`),
+  CONSTRAINT `id_venta_publica` FOREIGN KEY (`idPublica`) REFERENCES `usuario` (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Índices para tablas volcadas
+-- Dumping data for table `venta`
 --
 
---
--- Indices de la tabla `biblioteca`
---
-ALTER TABLE `biblioteca`
-  ADD PRIMARY KEY (`id_biblioteca`);
+LOCK TABLES `venta` WRITE;
+/*!40000 ALTER TABLE `venta` DISABLE KEYS */;
+/*!40000 ALTER TABLE `venta` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indices de la tabla `juegos`
---
-ALTER TABLE `juegos`
-  ADD PRIMARY KEY (`idjuego`);
-
---
--- Indices de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id_usuario`);
-
---
--- Indices de la tabla `venta`
---
-ALTER TABLE `venta`
-  ADD PRIMARY KEY (`id_venta`),
-  ADD UNIQUE KEY `id_publica` (`id_publica`),
-  ADD UNIQUE KEY `id_juegos` (`id_juegos`);
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `venta`
---
-ALTER TABLE `venta`
-  ADD CONSTRAINT `venta-juegos` FOREIGN KEY (`id_juegos`) REFERENCES `juegos` (`idjuego`),
-  ADD CONSTRAINT `venta-usuario` FOREIGN KEY (`id_publica`) REFERENCES `usuario` (`id_usuario`);
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-11-20  9:07:27
